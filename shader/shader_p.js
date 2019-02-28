1823ba3d8dceafd20d4deb9e2316983276776337
atiqu50 <atiqu50>
2019-03-01 00:14:36 +0900

    ファイル構成変更、不要ファイル削除

--- shader/shader_p.js
+++ shader/shader_p.js
@@ -1,27 +1,20 @@
 //******************************************************************************
-//* データ頂点シェーダー
+//* 頂点シェーダー(座標)
 //******************************************************************************
-var shader_data_vs = `
+var shader_p_vs = `
 	attribute vec3 position;
 	uniform mat4 mvpMatrix;
-	uniform float pointSize;
-	attribute vec4 color;
-	varying vec4 vColor;
-
 	void main(void){
 		gl_Position = mvpMatrix * vec4(position, 1.0);
-		gl_PointSize = pointSize;
-		vColor = color;
 	}
 `;
 
 //******************************************************************************
-//* データフラグメントシェーダー
+//* フラグメントシェーダー(色固定)
 //******************************************************************************
-var shader_data_fs = `
+var shader_p_fs = `
 	precision mediump float;
-	varying vec4 vColor;
 	void main(void){
-		gl_FragColor = vColor;
+		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 	}
 `;
--- shader/shader_pcs.js
+++ shader/shader_pcs.js
@@ -1,20 +1,27 @@
 //******************************************************************************
-//* データ頂点シェーダー
+//* 頂点シェーダー(座標、色、サイズ)
 //******************************************************************************
-var shader_p_vs = `
+var shader_pcs_vs = `
 	attribute vec3 position;
 	uniform mat4 mvpMatrix;
+	attribute float pointSize;
+	attribute vec4 color;
+	varying vec4 vColor;
+
 	void main(void){
 		gl_Position = mvpMatrix * vec4(position, 1.0);
+		gl_PointSize = pointSize;
+		vColor = color;
 	}
 `;
 
 //******************************************************************************
-//* データフラグメントシェーダー
+//* フラグメントシェーダー(色指定)
 //******************************************************************************
-var shader_p_fs = `
+var shader_pcs_fs = `
 	precision mediump float;
+	varying vec4 vColor;
 	void main(void){
-		gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+		gl_FragColor = vColor;
 	}
 `;
