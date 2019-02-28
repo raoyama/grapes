73fe702db75d927522bca3f5b90dc902b418823a
atiqu50 <atiqu50>
2019-02-28 23:55:42 +0900

    とりあえずブロック表示

--- graph_lib/line_data_script.js
+++ graph_lib/line_data_script.js
@@ -18,18 +18,111 @@ var g_scale_line_y_min			= undefined;	//Y軸目盛線　最小値(undefinedだ�
 //******************************************************************************
 var prg_data;
 var prg_lines;
+var prg_texture;
+
 var loc_data_position;
 var loc_data_color;
 var loc_data_mvpMatrix;
 var loc_data_pointSize;
 var loc_lines_position;
 var loc_lines_textureCoord;
+var loc_texture_position;
+var loc_texture_textureCoord;
+var loc_texture_mvpMatrix;
+var loc_texture_texture;
 
 //VBO
 var vbo_line_pos;
 var vbo_data_pos;
 var vbo_data_cls;
 var vbo_data_size;
+var vertex_position = [
+	// Front face
+	-1.0, -1.0,  1.0,
+	1.0, -1.0,  1.0,
+	1.0,  1.0,  1.0,
+	-1.0,  1.0,  1.0,
+
+	// Back face
+	-1.0, -1.0, -1.0,
+	-1.0,  1.0, -1.0,
+	1.0,  1.0, -1.0,
+	1.0, -1.0, -1.0,
+
+	// Top face
+	-1.0,  1.0, -1.0,
+	-1.0,  1.0,  1.0,
+	1.0,  1.0,  1.0,
+	1.0,  1.0, -1.0,
+
+	// Bottom face
+	-1.0, -1.0, -1.0,
+	1.0, -1.0, -1.0,
+	1.0, -1.0,  1.0,
+	-1.0, -1.0,  1.0,
+
+	// Right face
+	1.0, -1.0, -1.0,
+	1.0,  1.0, -1.0,
+	1.0,  1.0,  1.0,
+	1.0, -1.0,  1.0,
+
+	// Left face
+	-1.0, -1.0, -1.0,
+	-1.0, -1.0,  1.0,
+	-1.0,  1.0,  1.0,
+	-1.0,  1.0, -1.0,
+];
+var textures = [];
+var ibo;
+var index = [
+	0, 1, 2,      0, 2, 3,    // Front face
+	4, 5, 6,      4, 6, 7,    // Back face
+	8, 9, 10,     8, 10, 11,  // Top face
+	12, 13, 14,   12, 14, 15, // Bottom face
+	16, 17, 18,   16, 18, 19, // Right face
+	20, 21, 22,   20, 22, 23  // Left face
+];
+
+// テクスチャ座標
+var textureCoord = [
+	// Front face
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0,
+
+	// Back face
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	// Top face
+	0.0, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+
+	// Bottom face
+	1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+	1.0, 0.0,
+
+	// Right face
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0,
+	0.0, 0.0,
+
+	// Left face
+	0.0, 0.0,
+	1.0, 0.0,
+	1.0, 1.0,
+	0.0, 1.0,
+];
+var baseMatrix = m.identity(m.create());
 
 
 //******************************************************************************
@@ -38,8 +131,14 @@ var vbo_data_size;
 onload = function(){
 
 	init_canvas();
+	gl.enable(gl.CULL_FACE);	//カリング有効
 
 	init_gl();
+
+	vbo_textureCoord = create_vbo(textureCoord);
+
+	ibo = create_ibo(index);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
 	//データ
 	init_datas();
@@ -57,9 +156,24 @@ function draw_display() {
 	gl_clear();
 
 	set_mvp();
+	m.copy(mvpMatrix ,baseMatrix);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo1);
+	gl.enableVertexAttribArray(loc_texture_position);
+	gl.vertexAttribPointer(loc_texture_position, 3, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo_textureCoord);
+	gl.enableVertexAttribArray(loc_texture_textureCoord);
+	gl.vertexAttribPointer(loc_texture_textureCoord, 2, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	draw_block(0, 0, 0, 0);
 
 	//データ
 	draw_datas();
+
+//	draw_block(10, 10, 0, 1);
 
 	// コンテキストの再描画
 	gl.flush();
@@ -83,6 +197,27 @@ function init_gl() {
 	prg_lines = make_program_var(shader_p_vs, shader_p_fs);
 	loc_lines_position = gl.getAttribLocation(prg_lines, 'position');
 	loc_lines_mvpMatrix = gl.getUniformLocation(prg_lines, 'mvpMatrix');
+
+	//テクスチャー シェーダー
+	prg_texture = make_program_var(shader_texture_vs, shader_texture_fs);
+	loc_texture_position		= gl.getAttribLocation(prg_texture, 'position');
+	loc_texture_textureCoord	= gl.getAttribLocation(prg_texture, 'textureCoord');
+	loc_texture_mvpMatrix		= gl.getUniformLocation(prg_texture, 'mvpMatrix');
+	loc_texture_texture			= gl.getUniformLocation(prg_texture, 'texture');
+
+	// 深度テストを有効にする
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+
+	create_texture('texture/block1.png', 0);
+	create_texture('texture/block2.png', 1);
+
+	vbo1 = create_vbo(vertex_position);
+
+	vbo_textureCoord = create_vbo(textureCoord);
+
+	ibo = create_ibo(index);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
 }
 
@@ -131,5 +266,20 @@ function draw_datas() {
 
 
 }
+function draw_block(x,y,z, id) {
+	trans_size = 2.0;
+	a = [x,y,z];
+	trans = vec_mul([trans_size, trans_size, trans_size], a);
+	gl.useProgram(prg_texture);
 
+	//移動
+	gl.bindTexture(gl.TEXTURE_2D, textures[id]);
+//	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+//	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+	m.translate(baseMatrix, trans, mvpMatrix);
+	gl.uniformMatrix4fv(loc_texture_mvpMatrix, false, mvpMatrix);
+	gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+
+}
 
