e22eacc02628d37a5032a82f2e9125543de22b1e
atiqu50 <atiqu50>
2019-03-10 01:38:28 +0900

    空色に

--- script/GlCommon.js
+++ script/GlCommon.js
@@ -107,7 +107,7 @@ class GlCommon {
 	}
 
 	static gl_clear(){
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearColor(0.7, 0.7, 1.0, 1.0);
 		gl.clearDepth(1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	}
